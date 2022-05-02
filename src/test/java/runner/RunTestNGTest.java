bf5509da2e327cd2f5e791323bd4431ebe05c67e
Pavithri Fernando <pavithri.fernando@mcmedisoft.com>
2022-05-02 08:45:03 +0530

    Updating runner class with report feature

--- src/test/java/runner/RunTestNGTest.java
+++ src/test/java/runner/RunTestNGTest.java
@@ -3,7 +3,11 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/featureFiles", glue = "stepDefinitions",tags = "@validateUser")
+@CucumberOptions( plugin = {
+        "pretty",
+        "html:test-output.html",
+        "json:target/cucumber-report.json"
+},features = "src/test/resources/featureFiles", glue = "stepDefinitions",tags = "@validateUser")
 public class RunTestNGTest extends AbstractTestNGCucumberTests {
 
 }
